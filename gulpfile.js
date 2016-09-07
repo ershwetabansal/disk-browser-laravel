f0aeedc24846cb4d302c4f1b0e74f447f2ec6fb5
Shweta Bansal <ershwetabansal@gmail.com>
2016-09-08 00:15:48 +0100

    Update gulp file with correct task

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var source = require('vinyl-source-stream');
 var runSequence = require('run-sequence');
 
 
-gulp.task('heroku', function() {
+gulp.task('heroku:production', function() {
     return gulp.src('node_modules/disk-browser/dist/**/*')
     // Start piping stream to tasks!
         .pipe(gulp.dest('public/app/build/disk-browser'));
